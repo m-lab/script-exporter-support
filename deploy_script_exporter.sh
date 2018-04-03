17968e5c065c55e6b921603d1993b56ccdfb6801
nkinkade <nath@nkinka.de>
2018-04-03 09:06:48 -0600

    Removes cron from container in favor of using a systemd timer on the host VM. (#12)
    * Removes cron from container in favor of using a systemd timer on the host VM.

--- deploy_script_exporter.sh
+++ deploy_script_exporter.sh
@@ -8,7 +8,7 @@ set -x
 USAGE="Usage: $0 <project> <keyname>"
 PROJECT=${1:?Please provide project name: $USAGE}
 KEYNAME=${2:?Please provide an authentication key name: $USAGE}
-SCP_FILES="apply_tc_rules.sh apply_tc_rules.cron Dockerfile ndt_e2e.sh script_exporter.yml"
+SCP_FILES="apply_tc_rules.sh Dockerfile ndt_e2e.sh script_exporter.yml"
 IMAGE_TAG="m-lab/prometheus-script-exporter"
 GCE_ZONE="us-central1-a"
 GCE_NAME="script-exporter"
@@ -85,7 +85,7 @@ gcloud compute scp $SCP_FILES $GCE_NAME:~
 gcloud compute ssh $GCE_NAME --command "docker build -t ${IMAGE_TAG} ."
 
 # Start a new container based on the new/updated image.
-gcloud compute ssh $GCE_NAME --command "docker run --detach --restart always --publish 9172:9172 --cap-add NET_ADMIN ${IMAGE_TAG}"
+gcloud compute ssh $GCE_NAME --command "docker run --detach --restart always --publish 9172:9172 --name ${GCE_NAME} --cap-add NET_ADMIN ${IMAGE_TAG}"
 
 # Run Prometheus node_exporter in a container so we can gather VM metrics.
 gcloud compute ssh $GCE_NAME --command "docker run --detach --restart always --publish 9100:9100 --volume /proc:/host/proc --volume /sys:/host/sys prom/node-exporter --path.procfs /host/proc --path.sysfs /host/sys --no-collector.arp --no-collector.bcache --no-collector.conntrack --no-collector.edac --no-collector.entropy --no-collector.filefd --no-collector.hwmon --no-collector.infiniband --no-collector.ipvs --no-collector.mdadm --no-collector.netstat --no-collector.sockstat --no-collector.time --no-collector.timex --no-collector.uname --no-collector.vmstat --no-collector.wifi --no-collector.xfs --no-collector.zfs"
