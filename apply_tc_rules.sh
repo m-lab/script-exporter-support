6c22d1ee7ee4ff23c902857b47a0353f2a6761e5
Nathan Kinkade <kinkade@measurementlab.net>
2019-06-12 14:16:49 -0600

    Collects all ingress and egress filters in local variables before looping over them. More efficient than running tc 1000+ times.

--- apply_tc_rules.sh
+++ apply_tc_rules.sh
@@ -33,16 +33,18 @@ tc class add dev eth0 parent 1: classid 1:10 htb rate 5mbit || :
 # Add filters for each NDT IP address. egress traffic matching a destination IP
 # address of an NDT experiment gets queued into class id 1:10.  ingress traffic
 # with a source IP matching one of the NDT slivers gets policed at 50Kbps.
+egress_filters=$(tc filter show dev eth0 parent 1:)
+ingress_filters=$(tc filter show dev eth0 parent ffff:)
 
 for ip in $NDT_IPS; do
   HEX_IP=$(printf '%02x' ${ip//./ })
   # Only add the egress filter for this IP if it doesn't already exist.
-  if ! tc filter show dev eth0 parent 1: | grep "${HEX_IP}"; then
+  if ! echo "${egress_filters}" | grep "${HEX_IP}"; then
     tc filter add dev eth0 parent 1: protocol ip prio 1 \
         u32 match ip dst ${ip} flowid 1:10
   fi
   # Only add the ingress filter for this IP if it doesn't already exist.
-  if ! tc filter show dev eth0 parent ffff: | grep "${HEX_IP}"; then
+  if ! echo "${ingress_filters}" | grep "${HEX_IP}"; then
     tc filter add dev eth0 parent ffff: protocol ip prio 1 \
         u32 match ip src ${ip} police rate 50kbps burst 10k drop
   fi
